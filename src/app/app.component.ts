15f7335e3175bd68033545f3cc3911f5d1be1af1
Adrian Ferreres <ardiadrianadri@gmail.com>
2016-11-19 19:48:59 +0100

    Finish Volumen3-section-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../../node_modules/bootstrap/dist/css/bootstrap.css', '../../node_modules/bootstrap/dist/css/bootstrap-theme.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  ngOnInit() {
+    this.title = 'The value has changed';
+  }
+
+  pressButton () {
+    alert (`Value of title: ${this.title}`);
+  }
 }
