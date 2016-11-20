c6bf58f6418fa1245f167901d4843651595e2ab3
Adrian Ferreres <ardiadrianadri@gmail.com>
2016-11-20 09:04:24 +0100

    Tag output

--- src/app/searchForm/searchForm.component.ts
+++ src/app/searchForm/searchForm.component.ts
@@ -4,4 +4,22 @@ import { Component } from '@angular/core';
     selector: 'app-search',
     templateUrl: './searchForm.component.html'
 })
-export class SearchFormComponent {}
+export class SearchFormComponent {
+    private _pressOnReset () {
+        console.log('Button reset has been pressed');
+    }
+
+    private _pressOnSearch () {
+        console.log('Button search has been pressed');
+    }
+
+    public buttonPressed(title){
+        if (title === 'Reset') {
+            this._pressOnReset();
+        } else if (title === 'Search') {
+            this._pressOnSearch();
+        } else {
+            console.error('No event match with this button');
+        }
+    }
+}
