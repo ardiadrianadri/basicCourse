22a138f3f03dbda4e5b654f24c2b6cf14c6a3780
Adrian Ferreres <ardiadrianadri@gmail.com>
2016-12-18 09:34:12 +0100

    volume 2 section 2 005

--- src/app/common/auth.service.ts
+++ src/app/common/auth.service.ts
@@ -5,4 +5,23 @@ import { Md5 } from 'ts-md5/dist/md5';
 
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    getAuthQuery(): IAuthentication {
+        let result: IAuthentication = {
+            ts: new Date().getTime().toString(),
+            apiKey: environment.marvelPublicKey,
+            hash: null
+        };
+
+        let md5: Md5 = new Md5();
+
+        md5.appendStr(result.ts);
+        md5.appendStr(environment.marverPrivetKey);
+        md5.appendStr(environment.marvelPublicKey);
+
+        result.hash = md5.end().toString();
+
+        return result;
+    }
+}
