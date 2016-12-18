a0ef0e013749bed46826091a8ea0f0090a9aad2f
Adrian Ferreres <ardiadrianadri@gmail.com>
2016-12-18 17:40:02 +0100

    volume two section 11

--- src/app/searchForm/search.service.ts
+++ src/app/searchForm/search.service.ts
@@ -34,77 +34,6 @@ export class SearchService {
         key: 'events'
     }];
 
-    public lastResult: ISuperHero[] = [{
-        name: 'SuperDuck',
-        update: '01/01/2010',
-        comics: 100,
-        series: 100,
-        stories: 100,
-        events: 100
-    }, {
-        name: 'SuperPot',
-        update: '02/01/2010',
-        comics: 200,
-        series: 200,
-        stories: 200,
-        events: 200
-    }, {
-        name: 'SuperChair',
-        update: '03/01/2010',
-        comics: 300,
-        series: 300,
-        stories: 300,
-        events: 300
-    }, {
-        name: 'SuperStick',
-        update: '04/01/2010',
-        comics: 400,
-        series: 400,
-        stories: 400,
-        events: 400
-    }, {
-        name: 'SuperLazy',
-        update: '05/01/2010',
-        comics: 500,
-        series: 500,
-        stories: 500,
-        events: 500
-    }, {
-        name: 'Superspaghetti',
-        update: '06/01/2010',
-        comics: 600,
-        series: 600,
-        stories: 600,
-        events: 600
-    }, {
-        name: 'SuperCheese',
-        update: '07/01/2010',
-        comics: 700,
-        series: 700,
-        stories: 700,
-        events: 700,
-    }, {
-        name: 'SuperCarrot',
-        update: '08/01/2010',
-        comics: 800,
-        series: 800,
-        stories: 800,
-        events: 800,
-    }, {
-        name: 'SuperPen',
-        update: '09/01/2010',
-        comics: 900,
-        series: 900,
-        stories: 900,
-        events: 900,
-    }, {
-        name: 'SuperFork',
-        update: '10/01/2010',
-        comics: 110,
-        series: 110,
-        stories: 110,
-        events: 110,
-    }];
 
     constructor (private _authService: AuthService, private _http: Http) {}
 
@@ -131,7 +60,20 @@ export class SearchService {
 
             let totalPages = Math.floor( tableObj.totalNumber / size );
             tableObj.totalPages = ((tableObj.totalNumber % size) !== 0) ? ++totalPages : totalPages;
+            tableObj.data = responseObj.data.results.map( element => {
+                let superhero: ISuperHero = {
+                    name: element.name,
+                    update: element.modified,
+                    comics: element.comics.available,
+                    series: element.series.available,
+                    stories: element.stories.available,
+                    events: element.events.available
+                };
+
+                return superhero;
+            });
             return tableObj;
         })
+        .catch( error => Observable.throw(error));
     }
 }
