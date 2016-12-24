73596bb9f8089c1e580f7d6557a1c7265a7a17e5
Adrian Ferreres <ardiadrianadri@gmail.com>
2016-12-24 19:04:42 +0100

    Volume 2 section three 005

--- src/app/common/date.pipe.ts
+++ src/app/common/date.pipe.ts
@@ -2,9 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'dateformat'})
 export class DatePipe implements PipeTransform {
-    transform (date: string) {
+    transform (date: string, format: string) {
         let dateObj= new Date(date);
-        let result = `${(dateObj.getMonth() + 1)} / ${dateObj.getDate()} / ${dateObj.getFullYear()}`;
+        let result;
+
+        if (format === 'us') {
+            result = `${(dateObj.getMonth() + 1)} / ${dateObj.getDate()} / ${dateObj.getFullYear()}`;
+        } else {
+            result = `${dateObj.getDate()} / ${(dateObj.getMonth() + 1)} / ${dateObj.getFullYear()}`;
+        }
 
         return result;
     }
