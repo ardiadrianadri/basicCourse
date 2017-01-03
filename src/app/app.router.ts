2c92d4e97a36b0aec72644763d942879ab857424
Adrian Ferreres <ardiadrianadri@gmail.com>
2017-01-03 03:49:44 +0100

    volume 3 section 1 004

---
+++ src/app/app.router.ts
@@ -0,0 +1,11 @@
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { SearchFormComponent } from './searchForm/searchForm.component';
+import { DetailsComponent } from './details/details.component';
+
+export const routing: ModuleWithProviders = RouterModule.forRoot([
+    { path: '', component: SearchFormComponent },
+    { path: 'search', component: SearchFormComponent },
+    { path: 'details', component: DetailsComponent }
+]);
